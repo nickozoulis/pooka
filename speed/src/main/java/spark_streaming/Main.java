bd3f2f6812867054d45e75d59e171f25e0d07837
nickozoulis <nickolaszoulis@gmail.com>
2016-07-08 17:52:58 +0100

    playing with spark-streaming

--- speed/src/main/java/spark_streaming/Main.java
+++ speed/src/main/java/spark_streaming/Main.java
@@ -1,8 +1,16 @@
 package spark_streaming;
 
+import kafka.serializer.StringDecoder;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+
+import java.util.*;
 
 /**
  * Created by nickozoulis on 08/07/2016.
@@ -11,7 +19,39 @@ public class Main {
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("local[1]").setMaster("SparkStreaming");
-        JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(10));
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(10));
+
+        String topics = "youtube";
+        String brokers = "localhost:2181";
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, String> kafkaParams = new HashMap<>();
+        kafkaParams.put("metadata.broker.list", brokers);
+
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
+
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
+            @Override
+            public String call(Tuple2<String, String> tuple2) {
+                return tuple2._2();
+            }
+        });
+
+        
+
+
+        // Start the computation
+        jssc.start();
+        jssc.awaitTermination();
     }
 
 }
