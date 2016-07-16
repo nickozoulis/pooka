f5f803a38fcf4e41180e13b5c0f49d15fc965efd
nickozoulis <nickolaszoulis@gmail.com>
2016-07-16 21:15:56 +0100

    changed source simulator from feeding from a file to random generated strings

--- source-sim/src/main/java/Main.java
+++ source-sim/src/main/java/Main.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Random;
 
 /**
  * Created by nickozoulis on 18/06/2016.
  */
 public class Main {
+    // %s = string category, %d = int # of video views
+    private static String template = "LKh7zAJ4nwo\tTheReceptionist\t653\t%s\t424\t%d\t4.34\t1305\t744\tDjdA-5oKYFQ\tNxTDlnOuybo\tc-8VuICzXtU\tDH56yrIO5nI\tW1Uo5DQTtzc\tE-3zXq_r4w0\t1TCeoRPg5dE\tyAr26YhuYNY\t2ZgXx72XmoE\t-7ClGo-YgZ0\tvmdPOOd6cxI\tKRHfMQqSHpk\tpIMpORZthYw\t1tUDzOp10pk\theqocRij5P0\t_XIuvoH6rUg\tLGVU5DsezE0\tuO2kj6_D8B4\txiDqywcDQRM\tuX81lMev6_o";
+    private static String[] categories = {"Entertainment", ""};
+    private static Random r = new Random(1);
+
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
-            System.out.println("First argument should be the topic and second should be the input filepath");
+            System.out.println("First argument should be the topic and second the upper limit of string generator loop.");
             System.exit(0);
         }
 
@@ -30,18 +36,22 @@ public class Main {
         KafkaProducerFactory factory = new KafkaProducerFactory(p);
         IProducer producer = factory.getProducer();
 
-        BufferedReader br = null;
-        String line = "";
-        try {
-            br = new BufferedReader(new FileReader(args[1]));
+        int upper = Integer.parseInt(args[1]);
 
-            while ((line = br.readLine()) != null) {
-                producer.send(line);
+        for (int i=0; i<upper; i++) {
+            producer.send(strGenerator());
+
+            if (i % 1000 == 0) {
+                System.out.println(">>> " + i);
             }
-        } catch (Exception e) {
-        } finally {
-            producer.close();
-            br.close();
         }
+
+    }
+
+    private static String strGenerator() {
+        String category = categories[r.nextInt(categories.length - 1)];
+        int views = r.nextInt(10000000);
+
+        return String.format(template, category, views);
     }
 }
